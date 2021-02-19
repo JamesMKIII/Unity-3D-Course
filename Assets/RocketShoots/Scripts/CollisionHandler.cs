b0e509f8fe708aa00a0e3c22ef9786572a040c57
James <jamesdm.dev@gmail.com>
2021-02-19 04:20:28 -0500

    Add project files.

---
+++ Assets/RocketShoots/Scripts/CollisionHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Respawn":
+                Debug.Log("Launch Pad");
+                break;
+
+            case "Fuel":
+                Debug.Log("Fuel Pod");
+                break;
+
+            case "Finish":
+                Invoke("NextLevel", 1f);
+                break;
+
+            default:
+                Crashed();
+                break;
+        }
+    }
+
+    private void Respawn()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void Crashed()
+    {
+        Movement.isCrashed = true;
+        audioSource.Play();
+        Invoke("Respawn", 1f);
+    }
+}
---
+++ Assets/RocketShoots/Scripts/FuelPod.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelPod : MonoBehaviour
+{
+    Rigidbody rb;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.gameObject.SetActive(false);
+    }
+}
